dec2df7c5a6958a05708f5b24ef1cf2a56695445
Sithi <sithivignesh11@gmail.com>
2026-04-04 20:17:34 +0530

    detects endpoints returns Ai and Human %

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from transformers import pipeline
 
 app = FastAPI()
 
@@ -7,10 +8,30 @@ app = FastAPI()
 def health():
     return {"status": "ok"}
 
+detector = pipeline("text-classification", model="Hello-SimpleAI/chatgpt-detector-roberta")
+
 class TextInput (BaseModel):
     text: str
 
 @app.post("/detect")
 def detect(data: TextInput):
-    return {"received": data.text}
+    result = detector(data.text)
+    label = result[0]['label']
+    score = result[0]['score']
+
+    if label == "Human":
+        human_percent = score * 100
+        ai_percent = (1 - score) * 100
+    else:
+        ai_percent = score * 100
+        human_percent = (1 - score) * 100
+        
+    return {
+    "label": label,
+    "score": score,
+    "ai_percent": round(ai_percent, 2),
+    "human_percent": round(human_percent, 2)
+}
+
+
 
